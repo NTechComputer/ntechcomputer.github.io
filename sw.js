f5825ac460cd33a450bd8083396ca75ae65bf920
ntechComputer <ntech.author@gmail.com>
2022-02-26 04:24:35 +0600

    app lunch 0.1v

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('ntech').then((cache) => cache.addAll([
+        '/',
+        '/icon.svg',
+        '/logo.svg',
+        '/index.html',
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
+  
+  self.addEventListener('message', (event) => {
+    if (event.data === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+  });
